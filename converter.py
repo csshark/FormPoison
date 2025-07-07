9f2048e37f1c44c316b33fa1319fa4442a2e483b
Robert Strzoda <rstrzoda@zohomail.eu>
2025-07-07 23:30:57 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,25 +1,24 @@
 import json
 
+
 with open('input.txt', 'r') as file:
     lines = file.readlines()
 
 results = []
 
 for line in lines:
-    
+
     stripped_line = line.strip()
-    
-    
+
+
     result_entry = {
-        "inputField": f"", #leave it empty!!!
-        "category": "CATEGORY" #example "SQL"
+        "inputField": stripped_line,
+        "category": "Java" #allowed variables: "SQL" , "Java", "HTML"
     }
-    
-    
+
     results.append(result_entry)
 
 with open('results.json', 'w') as json_file:
     json.dump(results, json_file, indent=4, ensure_ascii=False)
 
-print("payloads written to results.json.")
-
+print("Payloads written to results.json.")
