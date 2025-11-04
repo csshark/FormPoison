322016263cf394fe8ffeabc180cd01c32bfbd250
Robert Strzoda <rstrzoda@zohomail.eu>
2025-11-04 18:31:43 +0100

    Create __init__.py

---
+++ functions/__init__.py
@@ -0,0 +1,6 @@
+from .csp_bypass import * 
+from .dynamic_payloads_gen import * 
+from .encoder_bypass import * 
+from .sanitizer_bypass import * 
+from .waf_bypass import * 
+from .size_overflow import * 
